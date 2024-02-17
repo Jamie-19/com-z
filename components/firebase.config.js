97b90c4b0e6f7de49d7bea37a72356dbd61f594f
Jamie-19 <mdl21cs057@mec.ac.in>
2024-02-17 15:54:11 +0530

    Update Firebase configuration

--- components/firebase.config.js
+++ components/firebase.config.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDOe8ppCefe37cqexq17_HDw2stpfncCY0",
-//   authDomain: "stuffed-252c0.firebaseapp.com",
-//   projectId: "stuffed-252c0",
-//   storageBucket: "stuffed-252c0.appspot.com",
-//   messagingSenderId: "126244669492",
-//   appId: "1:126244669492:web:90477a4d0144b5512e6261"
-// };
+//  // Your web app's Firebase configuration
+//  const firebaseConfig = {
+//    apiKey: "AIzaSyDOe8ppCefe37cqexq17_HDw2stpfncCY0",
+//    authDomain: "stuffed-252c0.firebaseapp.com",
+//    projectId: "stuffed-252c0",
+//     storageBucket: "stuffed-252c0.appspot.com",
+//    messagingSenderId: "126244669492",
+//    appId: "1:126244669492:web:90477a4d0144b5512e6261"
+//  };
 
 // // Initialize Firebase
 // export const app = initializeApp(firebaseConfig);
